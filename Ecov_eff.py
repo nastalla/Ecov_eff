d567affc58d23e80ccf606c9c93a2b55afad7492
Alla Nastovjak <alla@isp.nsc.ru>
2022-05-27 18:30:57 +0700

    Получаем массив неаддитивных энергий

--- Ecov_eff.py
+++ Ecov_eff.py
@@ -1,99 +1,32 @@
 import pandas as pd
-data = pd.read_csv('film_94_add.csv', sep = ';', skipinitialspace = True)
-#print(data.head()) #вывод первых 5 строк для проверки, верно ли загружен csv в dataframe
-#s = data[['s1', 's2']].sum(axis=1) #складываем столбцы под названием s1 и s2
+data_add = pd.read_csv('film_94_add.csv', sep = ';', skipinitialspace = True)
+#print(data_add.head()) #вывод первых 5 строк для проверки, верно ли загружен csv в dataframe
+#s = data_add[['s1', 's2']].sum(axis=1) #складываем столбцы под названием s1 и s2
 
 
-#print(data.columns.tolist()) # посмотреть названия столбцов
-#print(data) # вывод всего dataframe
+#print(data_add.columns.tolist()) # посмотреть названия столбцов
+#print(data_add) # вывод всего dataframe
 
 
-data1 = pd.read_csv('film_94_nonadd.csv', sep = ';', skipinitialspace = True)
+data_nonadd = pd.read_csv('film_94_nonadd.csv', sep = ';', skipinitialspace = True)
 
-result = data[['s2']] - data1[['s2']]
+for i in range (8, 13):
+    result = data_add.columns.get_lock()[i] - data_nonadd.columns.get_lock()[i]
+    #data_add.iloc[8, 13] #- data_nonadd.iloc[8, 13]
+
+
 print(result)
-
-'''if result!=0:
-    print(data[['State']])'''
-
-
-
-
-'''import csv
-S = []
-C = ''
-with open("film_94_add.csv", encoding='utf-8') as add_file:
-    # Создаем объект reader, указываем символ-разделитель ","
-    file_reader = csv.reader(add_file, delimiter = ";", skipinitialspace = True)
-    # Счетчик для подсчета количества строк и вывода заголовков столбцов
-    cnt = 0
-    # Считывание данных из CSV файла
-    for row in file_reader:
-        if cnt == 0:
-            # Вывод строки, содержащей заголовки для столбцов
-            print(f'Файл содержит столбцы: {", ".join(row)}')
-        else:
-            # Пишем строку в список
-            S.append(float(row))
-            print(row[1])
-        cnt += 1
-    print(S)
-    print(cnt)
     
-S = S.split()
-print(S)
+'''result = data_add[['s4']] - data_nonadd[['s4']] # находим разницу между колонками с энергиями
 
-    with open("film_94_nonadd.csv", encoding='utf-8') as non_add_file:
-        # Создаем объект reader, указываем символ-разделитель ","
-        file_reader1 = csv.reader(non_add_file, delimiter = ";")
-        #Счетчик для подсчета количества строк и вывода заголовков столбцов
-        cnt1 = 0
-        # Считывание данных из CSV файла
-        for row1 in file_reader1:
-            if cnt1 == 0:
-                # Вывод строки, содержащей заголовки для столбцов
-                print(f'Файл содержит столбцы: {", ".join(row1)}')
-            else:
-                # Вывод строк
-                C+=str(row1)
-            cnt1 += 1
+idx = result[result['s4']!=0].index # находим индекс отличающегося элемента
 
+print(idx)
 
+print(data_nonadd['s4'].iloc[idx]) # выводим неаддитивные значения из базы data1 по найденным индексам
 
-   
+S = ''
 
-
-
-
-
-add = []
-non_add = []
-cnt = 0
-with open ('film_94_add.csv') as inf_add:
-    for s in inf_add:
-        s = s.strip().replace(';', '') # удаляем служебные символы, удаляем ;
-        add.append(s)
-        #print(s)
-        cnt = cnt + 1
-
-for i in range (1, cnt):
-    add[i].split("  ")
-
-
-with open ('film_94_nonadd.csv') as inf_nonadd:
-    for line in inf_nonadd:
-        line = line.strip().replace(';', '')
-        non_add.append(line)
-
-for i in range (1, cnt):
-    non_add[i].split("  ")
-
-
-res = float(non_add[4][4]) - float(add[4][4])
-print(non_add[4])
-print(res)
-
-#for i in range (8, 13):
-    '''
-
+S = 'E = 4-' + str(data_nonadd['n2'].iloc[4])+'n2 = ' + str(data_nonadd['s4'].iloc[4])
+print(S)'''
 
