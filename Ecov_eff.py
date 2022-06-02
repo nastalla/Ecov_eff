9616ced71b93d32581a35bcc348448396db49aa4
nastalla <alla.nastovjak@ya.ru>
2022-06-02 17:51:39 +0700

    Найдены неаддитивные энергии, выводим их значение

--- Ecov_eff.py
+++ Ecov_eff.py
@@ -1,8 +1,6 @@
 import pandas as pd
 data_add = pd.read_csv('film_94_add.csv', sep = ';', skipinitialspace = True)
 #print(data_add.head()) #вывод первых 5 строк для проверки, верно ли загружен csv в dataframe
-#s = data_add[['s1', 's2']].sum(axis=1) #складываем столбцы под названием s1 и s2
-
 
 #print(data_add.columns.tolist()) # посмотреть названия столбцов
 #print(data_add) # вывод всего dataframe
@@ -10,26 +8,40 @@ data_add = pd.read_csv('film_94_add.csv', sep = ';', skipinitialspace = True)
 
 data_nonadd = pd.read_csv('film_94_nonadd.csv', sep = ';', skipinitialspace = True)
 
+# Вычитаем базу данных с аддитивными энергиями от базы с неаддитивными энергиями
 for index, row in data_add.iterrows():
     result = data_add - data_nonadd
 
-    
-print(result)
+def differ(result, df, S):
+  idx = result[result[S]!=0].index
+  print(df[S].iloc(idx))
 
-print(result[result.eq(0)].stack().reset_index())
+S = ''
+S = 's1'
+differ(result, data_nonadd, S)
 
 
+'''# Находим индекс отличающихся элементов
 
-    
-'''result = data_add[['s4']] - data_nonadd[['s4']] # находим разницу между колонками с энергиями
+idx1 = result[result['s1']!=0].index
+idx2 = result[result['s2']!=0].index
+idx3 = result[result['s3']!=0].index
+idx4 = result[result['s4']!=0].index
+idx5 = result[result['s5']!=0].index
+idx6 = result[result['s6']!=0].index
 
-idx = result[result['s4']!=0].index # находим индекс отличающегося элемента
-
-print(idx)
-
-print(data_nonadd['s4'].iloc[idx]) # выводим неаддитивные значения из базы data1 по найденным индексам
+print(data_nonadd['s1'].iloc[idx1])
+print(data_nonadd['s2'].iloc[idx2])
+print(data_nonadd['s3'].iloc[idx3])
+print(data_nonadd['s4'].iloc[idx4])
+print(data_nonadd['s5'].iloc[idx5])
+print(data_nonadd['s6'].iloc[idx6])
 
 S = ''
 
-S = 'E = 4-' + str(data_nonadd['n2'].iloc[4])+'n2 = ' + str(data_nonadd['s4'].iloc[4])
-print(S)'''
+for i in idx1:
+    S = 'E = 1-' + str(data_nonadd['n1'].iloc[i])+ '*n1-' + str(data_nonadd['n2'].iloc[i])+ '*n2-'+ str(data_nonadd['n3'].iloc[i]) + '*n3-' + str(data_nonadd['n4'].iloc[i]) + '*n4-'+ str(data_nonadd['n5'].iloc[i]) + '*n5-' + str(data_nonadd['n6'].iloc[i])+'='+str(data_nonadd['s1'].iloc[i])
+    print(S)
+'''
+
+    
