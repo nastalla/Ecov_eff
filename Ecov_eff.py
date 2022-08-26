546f3e32e5c5277edb3f9d0fdc74067fa0eb587a
nastalla <alla.nastovjak@ya.ru>
2022-08-25 20:51:01 -0700

    Скрипт готов - названия сортов берутся из файла *.txt, найденные отличия между файла с аддитивными и неаддитивными энергиями записываются в файл (путь к файлу запрашивается)

--- Ecov_eff.py
+++ Ecov_eff.py
@@ -1,8 +1,18 @@
 import pandas as pd
 from tkinter import *
 from tkinter import filedialog
-from tkinter import messagebox
 
+
+# Функция записи неаддитивных энергий файл
+def write_file(lines, m):
+    f = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
+    if m != 0:
+        for line in lines:
+            f.write(line + "\n")
+        f.close()
+    else:
+        f.write("Неаддитивные энергии не найдены")
+        f.close()
 
 # Функция для поиска индексов неаддитивных значений и вывода самих значений
 def differ():
@@ -25,34 +35,37 @@ def differ():
 
         # ищем окружение для неаддитивных состояний
         for i in idx: # перебираем все строки в базе индексов ненулевых элементов
-            E = 'E(' + sort_inputs[j-1].get('1.0', 'end-1c')
-            #E = 'E' + str(j) 
+            E = 'E(' + sort_inputs[j-1] 
             for k in range(1, (n+1)):
                 # условие - если количество соседей не равно 0, то выводим это число соседей:
                 if data_nonadd['n'+str(k)].iloc[i]!=0:
-                    #E = E + '-' + str(data_nonadd['n'+str(k)].iloc[i])+ '*n' + str(k)
-                    E = E + '-' + str(data_nonadd['n'+str(k)].iloc[i]) + sort_inputs[k-1].get('1.0', 'end-1c')
+                    if data_nonadd['n'+str(k)].iloc[i] > 1:
+                        E = E + '-' + str(data_nonadd['n'+str(k)].iloc[i]) + sort_inputs[k-1]
+                    else:
+                        E = E + '-' + sort_inputs[k-1]
             E = E + ') = '+str(data_nonadd[S].iloc[i]) # добавляем значение неаддитивной энергии для данного окружения
             lines.append(E)
             m = m + 1
         S = 's' # обнуляем название колонки
         
     # Записываем массив неаддитивных энергий в файл
-    f = open('text.txt', 'w')
-    if m != 0:
-        for line in lines:
-            f.write(line + "\n")
-        f.close()
-    else:
-        f.write("Неаддитивные энергии не найдены")
-        f.close()
-
+    write_file(lines, m)
+    
 
 # GUI
 window = Tk()
 window.title("Поиск неаддитивных энергий")
-window.geometry("1000x300")
+window.geometry("600x300")
 
+# Кнопка выбора файла с названиями сортов
+def click_sort():
+    global sort_inputs
+    sort_inputs = []
+    sort_file = filedialog.askopenfilename() # путь к файлу (строка)
+    with open(sort_file, 'r') as f:
+        for line in f.readlines():
+            sort_inputs.append(line.rstrip('\n')) # rstrip - для удаления символа перевода строки
+    
 # Кнопка выбора файла с аддитивными энергиями
 def click_add():
     global file_add
@@ -66,69 +79,62 @@ def click_nonadd():
 # Кнопка для проведения расчета и записи результата в файл
 def click_result():
     differ()
-    MB = messagebox.showinfo("", "Готово!")
+    label = Label(text = 'Готово!')
+    label.config(font = ("Arial", 16), justify = CENTER)
+    label.place(x = 270, y = 250)
 
+# Основная функция GUI
+def sort_input(event):
+    # Выберите файл с названиями сортов
+    label1 = Label(text = "2. Выберите файл с названиями сортов")
+    label1.config(font = ("Arial", 12), justify = LEFT)
+    label1.place(x = 10, y = 45)
 
-# Функция считывания названий сортов из окошек
-def sort_input():
+    # Кнопка выбора файла с названиями сортов
+    btn_sort = Button(window, text = "Выбрать...", font = ("Arial", 12), height = 1, width = 10, command = click_sort)
+    btn_sort.place(x = 450, y = 45)
+    
     # Выберите файл с аддитивными энергиями
-    label2 = Label(text = "2. Выберите файл с аддитивными энергиями")
+    label2 = Label(text = "3. Выберите файл с аддитивными энергиями")
     label2.config(font = ("Arial", 12), justify = LEFT)
-    label2.place(x = 500, y = 10)
+    label2.place(x = 10, y = 80)
 
     # Кнопка выбора файла с аддитивными энергиями
     btn_add_file = Button(window, text = "Выбрать...", font = ("Arial", 12), height = 1, width = 10, command = click_add)
-    btn_add_file.place(x = 860, y = 5)
+    btn_add_file.place(x = 450, y = 85)
 
 
     # Выберите файл с неаддитивными энергиями
-    label3 = Label(text = "3. Выберите файл с неаддитивными энергиями")
+    label3 = Label(text = "4. Выберите файл с неаддитивными энергиями")
     label3.config(font = ("Arial", 12), justify = LEFT)
-    label3.place(x = 500, y = 45)
+    label3.place(x = 10, y = 120)
 
     # Кнопка выбора файла с неаддитивными энергиями
     btn_nonadd_file = Button(window, text = "Выбрать...", font = ("Arial", 12), height = 1, width = 10, command = click_nonadd)
-    btn_nonadd_file.place(x = 860, y = 45)
+    btn_nonadd_file.place(x = 450, y = 120)
 
 
     # Записать неаддитивные энергии в файл
-    label4 = Label(text = "4. Записать неаддитивные энергии в файл")
+    label4 = Label(text = "5. Записать неаддитивные энергии в файл")
     label4.config(font = ("Arial, 12"), justify = LEFT)
-    label4.place(x = 500, y = 80)
+    label4.place(x = 10, y = 160)
 
     btn_result = Button(window, text = "Записать", font = ("Arial", 12), height = 1, width =10, command = click_result)
-    btn_result.place(x = 860, y = 80)
-
-    
-# Функция для создания окошек для ввода названий сортов
-def sort(event):
-    global sort_inputs
-    sort_inputs = [] # массив сортов
-    n = int(number_sort_text.get())
-    for i in range (1, (n+1)):
-        sort_text = Text(window, width = 10, height = 1)
-        sort_text.place(x = 150, y = 30+i*30)
-        sort_inputs.append(sort_text)
-        label_sort = Label(text = "Сорт " + str(i))
-        label_sort.place(x = 30, y = 30 + 30*i)
-    btn_sort = Button(window, text = "Далее", command = sort_input)
-    btn_sort.place(x = 150, y = 270)    
+    btn_result.place(x = 450, y = 160)
         
 
-        
+# Вход в программу        
 # Введите число сортов
-label1 = Label(text = "1. Введите число сортов ")
+label1 = Label(text = "1. Введите число сортов и нажмите Enter ")
 label1.config(font = ("Arial", 12), justify = LEFT)
 label1.place(x = 10, y = 10)
 
 # Поле ввода для числа сортов
 number_sort_text = Entry()
-number_sort_text.place(x = 200, y = 15)
+number_sort_text.place(x = 450, y = 15)
 number_sort_text.focus_set()
 
-number_sort_text.bind('<Return>', sort)
-
-
+number_sort_text.bind('<Return>', sort_input)
 
 window.mainloop()
 
